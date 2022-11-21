87cf1e6fbc9c684d172fb7b73639bb1a6043af41
Ramses-Moreno <ram.ramses21sentinela@gmail.com>
2022-11-20 21:01:52 -0800

    for

---
+++ Secuencia_Repeticion_For.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+
+ main()
+{
+
+    int i=0;
+    for (i=0; i<10; i++)
+    {
+        printf("w");
+    }
+    getchar();
+}
